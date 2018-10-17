8016358496822b4b29554eaa9d42dca1cb1c1d1d
Tarutar <43999667+Tarutar@users.noreply.github.com>
2018-10-17 00:55:52 -0500

    Add files via upload

---
+++ Lab4.ps1
@@ -0,0 +1,11 @@
+﻿$fileserver = Import-Csv C:\employeelist.csv
+New-Item -Path C:\ -ItemType Directory -name Fileserver
+foreach ($name in $fileserver){
+    New-Item -ItemType Directory -Path C:\Fileserver\ -Name $name.Name.Replace(' ','').ToLower()
+
+    }
+$directory = Get-ChildItem C:\Fileserver
+foreach ($employee in $directory){
+    New-Item -Path C:\Fileserver\$employee -Name Hello.txt -Value "Welcome message!"
+
+    }
